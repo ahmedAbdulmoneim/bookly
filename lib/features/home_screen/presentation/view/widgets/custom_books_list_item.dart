205d0b89ef3ae21cfbaa2f913d6daba23d000eb1
ahmed <3lbashmohands@gmail.com>
2024-08-03 00:28:14 +0300

    using cached network image

--- lib/features/home_screen/presentation/view/widgets/custom_books_list_item.dart
+++ lib/features/home_screen/presentation/view/widgets/custom_books_list_item.dart
@@ -1,28 +1,25 @@
-import 'package:bookly/core/resources/assets_manager.dart';
+import 'package:bookly/core/resources/values_manager.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
-import '../../../../../core/resources/values_manager.dart';
-
 class CustomBookImage extends StatelessWidget {
-  const CustomBookImage({
-    super.key,
-    this.bookImage
-  });
-final String? bookImage;
+  const CustomBookImage({super.key, this.bookImage});
+
+  final String? bookImage;
+
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 0.66,
-      child: Container(
-        margin: const EdgeInsets.only(right: AppMargin.m10),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(AppSize.s16),
-          color: Colors.red,
-          image:  DecorationImage(
+    return Padding(
+      padding: const EdgeInsets.only(right: AppPadding.p20),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(AppSize.s16),
+        child: AspectRatio(
+          aspectRatio: 0.66,
+          child: CachedNetworkImage(
             fit: BoxFit.fill,
-            image: NetworkImage(
-              bookImage?? "http://books.google.com/books/content?id=9GwrmHRl490C&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
-            ),
+            errorWidget: (context, url, error) => const Icon(Icons.error),
+            imageUrl: bookImage ??
+                "https://books.google.com/books/content?id=9GwrmHRl490C&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
           ),
         ),
       ),
